9966000028d3813c8689171c732bc19fa40a45dc
dante <singularis314@gmail.com>
2025-01-15 21:22:28 +0000

    Improve ui kafka consumer

--- chater_ui/kafka_consumer.py
+++ chater_ui/kafka_consumer.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from datetime import time
 
 from confluent_kafka import Consumer, KafkaError, KafkaException
 
@@ -19,15 +20,19 @@ def create_consumer(topics):
             {
                 "bootstrap.servers": os.getenv("BOOTSTRAP_SERVER"),
                 "group.id": "chater",
-                "auto.offset.reset": "latest",
+                "auto.offset.reset": "earliest",
                 "enable.auto.commit": True,
+                "max.poll.interval.ms": 300000,
             }
         )
 
         def on_assign(consumer, partitions):
             logger.info(f"Assigned partitions: {partitions}")
 
-        consumer.subscribe(topics, on_assign=on_assign)
+        def on_revoke(consumer, partitions):
+            logger.info(f"Partitions revoked: {partitions}")
+
+        consumer.subscribe(topics, on_assign=on_assign, on_revoke=on_revoke)
         logger.info(f"Subscribed to topics: {topics}")
         return consumer
     except KafkaException as e:
@@ -38,17 +43,25 @@ def create_consumer(topics):
 def consume_messages(consumer):
     try:
         while True:
-            msg = consumer.poll(2.0)
+            msg = consumer.poll(1.0)
             if msg is None:
                 continue
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
-                    logger.info(f"End of partition reached {msg.partition()}")
+                    logger.info(
+                        f"End of partition reached for topic {msg.topic()} partition {msg.partition()} offset {msg.offset()}"
+                    )
+                    continue
+                elif msg.error().code() == KafkaError.BROKER_NOT_AVAILABLE:
+                    logger.error("Broker not available. Retrying in 5 seconds...")
+                    time.sleep(5)
+                    continue
+                elif msg.error().code() == KafkaError.INVALID_MSG_SIZE:
+                    logger.error(f"Message too large: {msg.error()}")
                     continue
                 else:
                     logger.error(f"Consumer error: {msg.error()}")
                     continue
-
             logger.info(f"Consumed message: {msg.key()} - {msg.value()}")
             yield msg
     except KafkaException as e:
