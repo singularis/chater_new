42487026aa760e343454fc83238439a61a33c16c
dante <singularis314@gmail.com>
2025-05-24 14:04:01 +0000

    Adjust promt

--- chater_ui/deploy.sh
+++ chater_ui/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build -t singularis314/chater-ui:0.4 .
-docker push singularis314/chater-ui:0.4
+docker build -t singularis314/chater-ui:0.5 .
+docker push singularis314/chater-ui:0.5
 kubectl rollout restart -n chater-ui deployment chater-ui
